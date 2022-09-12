838eb453a4b154abe4b33ab4a3d6752814b59eb2
SevindzhK <sevinge0709@gmail.com>
2022-09-12 16:23:49 +0300

    Added Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Enter five-digit number: ");
+string number = Console.ReadLine(); 
+
+
+void Palindrome5(string text)
+{
+    int length = text.Length;
+    if (length == 5) 
+    {
+        int start = 0;
+        int end = length - 1; 
+
+        while (start < end)
+        {
+            if (text[start] == text[end])
+            {
+                start++;
+                end--;
+            }
+            else
+            {
+                Console.WriteLine("The number isn't palindrom");
+                return;
+            }
+        }
+     Console.WriteLine("The number is palindrom");
+    } 
+    else 
+    {
+        Console.WriteLine("You entered incorrect number. Enter 5-digit number");
+    }
+}
+Palindrome5(number);
